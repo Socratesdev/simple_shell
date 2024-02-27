ac3ceb22be14acc42edaea12ee90b6ca5c4d3370
Socratesdev <makmanuel101@gmail.com>
2024-02-27 18:42:35 +0300

    i dont know update something

---
+++ exec.c
@@ -0,0 +1,14 @@
+#include "shelllib.h"
+
+int main(void)
+{
+	char *argv[] = {"/bin/ls", "-l", "/usr/", NULL};
+
+    printf("Before execve\n");
+    if (execve(argv[0], argv, NULL) == -1)
+    {
+        perror("Error:");
+    }
+    printf("After execve\n");
+    return (0);
+}
